7d0d62f0c56bd28595f52c034640a56c1936813b
saicharan0112 <saicharan00112@gmail.com>
2023-01-09 09:52:38 +0530

    final test of this experiment

--- utils/build_csv.py
+++ utils/build_csv.py
@@ -14,34 +14,42 @@ for i in folders:
     inverter=config[2]
 
     # open drc report and look whether the count is blank or not - basically the first line of the drc report should end with " " for the drc errors to be 0
-    with open(Direc+i+"/6_final_drc.rpt") as lines:
-        for line in lines:
-            if "count: " in line:
-                drc="clean"
-            else:
-                drc="not clean"
-            break
+    if os.path.exists(Direc+i+"/6_final_drc.rpt"):
+        with open(Direc+i+"/6_final_drc.rpt") as lines:
+            for line in lines:
+                if "count: " in line:
+                    drc="clean"
+                else:
+                    drc="not clean"
+                break
+    else:
+        print("Error - found no drc rpt")
 
     # open lvs report and look for the line "Final result: Circuits match uniquely.". If found, lvs is clean, else it is failed
-    with open(Direc+i+"/6_final_lvs.rpt") as lines:
-        data=lines.read()
-        if "Final result: Circuits match uniquely." in data:
-            lvs="clean"
-        else:
-            lvs="not clean"
+    if os.path.exists(Direc+i+"/6_final_drc.rpt"):
+        with open(Direc+i+"/6_final_lvs.rpt") as lines:
+            data=lines.read()
+            if "Final result: Circuits match uniquely." in data:
+                lvs="clean"
+            else:
+                lvs="not clean"
+    else:
+        print("Error - found not lvs rpt")
     
-    # open all folders, get the config, open prePEX_sim_output and extract temp-freq-power-error.  
-    with open(Direc+i+"/prePEX_sim_result") as lines:
-        for line in lines:
-            data=line.split()
-            if not "/" in data[0] and (data[0].isdigit() or data[0].startswith("-")):
-                values.append([header,inverter]+data)
+    # open all folders, get the config, open prePEX_sim_output and extract temp-freq-power-error. 
+    if os.path.exists(Direc+i+"/prePEX_sim_result"): 
+        with open(Direc+i+"/prePEX_sim_result") as lines:
+            for line in lines:
+                data=line.split()
+                if not "/" in data[0] and (data[0].isdigit() or data[0].startswith("-")):
+                    values.append([header,inverter]+data)
+    else:
+        print("Error - found not sim rpt")
     
     # combine all results in single dictionary
     result.append(values)
     values=[]
 
-print(result)
 # dump results in a csv file
 fields=["header","inverter","temp","freq","power","error"]
 with open("data.csv","w") as f:
